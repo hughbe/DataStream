6136e69031194a6518cdc3316d79ca83a0a2acfd
Hugh Bellamy <hughbellars@gmail.com>
2020-11-04 14:08:48 +0000

    Add Sequence hexString

--- Sources/DataStream/BinaryInteger+HexString.swift
+++ Sources/DataStream/BinaryInteger+HexString.swift
@@ -8,6 +8,16 @@
 
 public extension BinaryInteger {
     var hexString: String {
+        if self < 0 {
+            return String(self)
+        }
+
         return "0x\(String(self, radix: 16, uppercase: true).padLeft(toLength: MemoryLayout<Self>.size * 2, withPad: "0"))"
+    }
+}
+
+public extension Sequence where Element: FixedWidthInteger {
+    var hexString: String {
+        return "[\(map { $0.hexString }.joined(separator: ", "))]"
     }
 }
